5f342df6d2b4fbce003e0689974f65c3761a2df8
Pravallika Palla <153248549+PallaPravallika4@users.noreply.github.com>
2024-08-07 11:21:26 -0400

    Create ErrorViewModel.cs

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentInformation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
