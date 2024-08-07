0e2443074689c518bdafbbd1a67ecf11386f7081
Pravallika Palla <153248549+PallaPravallika4@users.noreply.github.com>
2024-08-07 11:18:37 -0400

    Create Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentInformation.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int marks1 { get; set; }
+        public int marks2 { get; set; }
+        public int marks3 { get; set; }
+        public int marks4 { get; set; }
+        public int marks5 { get; set; }
+        public int marks6 { get; set; }
+
+
+    }
+}
